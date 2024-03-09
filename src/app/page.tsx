6ab2d52711436f610cd9c04487e4dae1228008b8
didi2424 <95207941+didi2424@users.noreply.github.com>
2024-03-10 00:28:56 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client"
 import Camea from "./cameraon";
 import CameraStream from "./camerastream4";
-import VideoStream from "./videostream";
 export default function Home() {
- 
   return (
     <div>
-   <Camea/>
-  </div>
+      <Camea />
+    </div>
   );
 }
